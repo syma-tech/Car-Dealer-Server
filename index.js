a3ae0fb17d18c8d2ae79aa55b200bee41895c051
“syma-tech” <symarahman17@gmail.com>
2024-06-13 17:19:52 +0600

    another

--- index.js
+++ index.js
@@ -28,9 +28,25 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const userCollection = client.db("carDealerDB").collection("users");
     const carsCollection = client.db("carDealerDB").collection("cars");
     const reviewsCollection = client.db("carDealerDB").collection("reviews");
     const orderCollection = client.db("carDealerDB").collection("orders");
+
+    // user apis here
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return { message: "User already exists", insertedId: null };
+      }
+      const result = await userCollection.insertOne(user);
+      console.log(user);
+      console.log(result);
+      res.send(result);
+    });
 
     // cars apis here
 
@@ -44,7 +60,6 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await carsCollection.findOne(query);
-      console.log(result);
       res.send(result);
     });
 
@@ -54,12 +69,31 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result);
+    });
+
     // orders apis here
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await orderCollection.find(query).toArray();
+      res.send(result);
+    });
 
     app.post("/orders", async (req, res) => {
-      const order = req.body;
+      const order = req.body.email;
       const result = await orderCollection.insertOne(order);
-      console.log(result);
+      // console.log(result);
+    });
+
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
